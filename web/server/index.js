f9bcf04648779c3080adccb858a16bc1f5bdfb3d
Graeme Britz <grbritz@gmail.com>
2015-01-26 12:25:45 -0800

    hello world server & annotator

--- web/server/index.js
+++ web/server/index.js
@@ -1,4 +1,19 @@
-import express = require('express');
-import session = require('express-session');
-import http = require('http');
-
+/// <reference path='./ts-definitions/node.d.ts' />
+/// <reference path='./ts-definitions/express.d.ts' />
+var express = require('express');
+// import session = require('express-session');
+var http = require('http');
+var STATIC_PATH = '../client';
+var app = express();
+var server = new http.Server(app);
+// Use the static path directory to serve our website
+// app.use(express.static(__dirname + STATIC_PATH));
+app.get("/", function (req, res) {
+    res.send("We're running.");
+});
+server.listen(8080);
+// var server = app.listen(8080, function () {
+//   var host = server.address().address
+//   var port = server.address().port
+//   console.log('Example app listening at http://%s:%s', host, port)
+// }) 
--- web/server/index.ts
+++ web/server/index.ts
@@ -1,4 +1,20 @@
-import express = require('express');
-import session = require('express-session');
-import http = require('http');
+/// <reference path='./ts-definitions/node.d.ts' />
+/// <reference path='./ts-definitions/express.d.ts' />
 
+var express = require('express');
+// import session = require('express-session');
+var http = require('http');
+// var STATIC_PATH : string = '../client';
+
+
+var app = express();
+var server = new http.Server(app);
+// Use the static path directory to serve our website
+// app.use(express.static(__dirname + STATIC_PATH));
+
+
+app.get("/", function(req, res) {
+  res.send("We're running.");
+});
+
+server.listen(8080);
