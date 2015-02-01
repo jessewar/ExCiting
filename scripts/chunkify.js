5a0f703ec5dd90c35ec3eca9c6d674b79dcda50c
jessewar <jesse.warren@live.com>
2015-02-01 13:16:13 -0800

    updated chunkify.js

--- scripts/chunkify.js
+++ scripts/chunkify.js
@@ -1,17 +1,11 @@
-var parseString = require('xml2js').parseString;
 var fs = require("fs");
-var xpath = require("xpath.js");
+var xpath = require("xpath");
 var dom = require("xmldom").DOMParser;
 fs.readFile("./test.xml", "utf8", function(err, data) {
   if (err) { console.log(err); }
-  // var startString = "<citationList>";
-  // var endString = "</citationList>";
-  // var citations = data.substring(data.indexOf(startString), data.indexOf(endString) + endString.length);
-  // parseString(citations, function(err, result) {
-  //   if (err) { console.log(err); }
-  //   console.log(JSON.stringify(result.citationList[0], undefined, 2));
-  // });
   var xml = new dom().parseFromString(data);
-  var nodes = xpath("//citation", xml);
-  console.log(nodes[0]);
+  var nodes = xpath.select("//citation", xml);
+  for (var i = 0; i < nodes.length; i++) {
+    console.log(nodes[i].toString());
+  }
 });
