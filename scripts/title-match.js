5f610b6fa1a5a18999da6576ec39c563563d83af
jessewar <jesse.warren@live.com>
2015-02-10 16:35:57 -0800

    updated title match script

--- scripts/title-match.js
+++ scripts/title-match.js
@@ -4,31 +4,51 @@ var dom = require("xmldom").DOMParser;
 
 // Build map of paper_title -> paper_id
 var map = {};
-var papers = fs.readFileSync("paper_ids.txt", "utf8").split("\n");
-for (var i = 0; i < papers.length; i++) {
-  var tokens = papers[i].split("\t");
+var paper_ids = fs.readFileSync("paper_ids.txt", "utf8").split("\n");
+for (var i = 0; i < paper_ids.length; i++) {
+  var tokens = paper_ids[i].split("\t");
   var id = tokens[0];
   var title = tokens[1];
-  map[title] = id;
+  if (title != undefined && id != undefined) {
+    map[title.toLowerCase()] = id;
+  }
 }
 
-// W04-1013
-
-var files = fs.readdirSync("/home/jesse/Classes/CSE454/ExCiting/data/paper_subset/chunks");
-var match = 0;
-files.forEach(function(file) {
-  var filecontents = fs.readFileSync("./paper-dir/output/" + file, "utf8");
-  var xml = new dom().parseFromString(filecontents);
-  if (xml != undefined) {
-    var citations = xpath.select("//citation", xml);
-    for (var i = 0; i < citations.length; i++) {
-      var title = xpath.select("title/text()[1]", citations[i]).toString();
-      title = title.substring(0, title.length-1);  // take off period
-      if (map[title] != undefined && match[title] === "W04-1013") {
-        match++;
-        console.log("FOUND: " + map[title] + "source: " + file);
+// Build list of most heavily cited papers
+var papers = fs.readFileSync("../../papers_above_threshold.txt", "utf8").split("\n");
+var parsed_papers = fs.readdirSync("/home/jesse/Classes/CSE454/ExCiting/data/paper_subset/chunks");
+var counts = {};  // map of paper id to number of times it was referenced from papers within papers_above_threshold using current title matching algorithm
+for (var i = 0; i < 20; i++) {
+  var paper = papers[i];
+  var match = 0;
+  parsed_papers.forEach(function(parsed_paper) {
+    try {
+      var parsed_paper_contents = fs.readFileSync("./" + parsed_paper, "utf8");
+      var xml = new dom().parseFromString(parsed_paper_contents);
+      if (xml != undefined) {
+        var citations = xpath.select("//citation", xml);
+        if (citations != undefined) {
+          for (var i = 0; i < citations.length; i++) {
+            var title = xpath.select("title/text()[1]", citations[i]).toString();
+            if (title != undefined) {
+              title = title.substring(0, title.length-1).toLowerCase();  // take off period and do case insensitive comparison
+              if (map[title] != undefined) {
+                var target_id = paper;
+                if (map[title].localeCompare(target_id) === 0) {
+                  match++;
+                }
+              }
+            }
+          }
+        }
       }
+    } catch (err) {
+      console.log(err);
     }
-  }
-});
-console.log(match + " title matches out of " + count + " titles");
+  });
+  counts[paper] = match;
+}
+
+for (var key in counts) {
+  console.log(key + " " + counts[key]);
+}
