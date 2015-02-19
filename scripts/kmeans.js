cb4bd4416f749ede9a62b63862a23bd3675e8e09
jessewar <jesse.warren@live.com>
2015-02-18 17:27:51 -0800

    kmeans with bag of words vectors

--- scripts/kmeans.js
+++ scripts/kmeans.js
@@ -1,58 +1,60 @@
+var fs = require("fs");
 var MongoClient = require("mongodb");
 var stopwords = require("stopwords").english;
 var clusterfck = require("clusterfck");
 
-var dictionary = [];
-var vectors = [];
-var vectorToString = {};
 var dbPath = "mongodb://localhost/mongodata";
 MongoClient.connect(dbPath, function(err, db) {
   var extractionCollection = db.collection("re_sentence_extractions");
   var clusterCollection = db.collection("clusters");
+  var papers_above_threshold = fs.readFileSync("./papers_above_threshold.txt", "utf8").split("\n");
+  papers_above_threshold.forEach(function(paper_id) {
+    extractionCollection.find({cited_paper:paper_id}).toArray(function(err, documents) {
+      var dictionary = [];
+      populateDictionary(documents, dictionary);
+      var vectors = [];
+      var vectorToString = {};
+      if (!err && documents.length > 0) {  // must have extractions associated with the paper
+        documents.forEach(function(extraction_doc) {
+          var vector = [];
+          var extraction = extraction_doc.extraction;
+          var extraction_words = extraction.toLowerCase().split(" ");
+          dictionary.forEach(function(dictionary_word) {
+            if (extraction_words.indexOf(dictionary_word) >= 0) {
+              vector.push(1);
+            } else {
+              vector.push(0);
+            }
+          });
+          vectors.push(vector);
+          vectorToString[vector] = extraction;
+        });
 
-
-  extractionCollection.find({cited_paper:"W96-0213"}).toArray(function(err, documents) {
-    if (!err) {
-      populateDictionary(documents);
-      documents.forEach(function(extraction_doc) {
-        var vector = [];
-        var extraction = extraction_doc.extraction;
-        var extraction_words = extraction.toLowerCase().split(" ");
-        dictionary.forEach(function(dictionary_word) {
-          if (extraction_words.indexOf(dictionary_word) >= 0) {
-            vector.push(1);
-          } else {
-            vector.push(0);
+        // group vectors into k clusters
+        var k = 3;
+        var clusters = clusterfck.kmeans(vectors, k);
+        var cluster_doc = {"cited_paper":paper_id};
+        for (var i = 0; i < k; i++) {
+          var cluster = clusters[i];
+          var string_cluster = [];
+          if (cluster != undefined) {
+            cluster.forEach(function(vector) {
+              string_cluster.push(vectorToString[vector]);
+            });
+            cluster_doc[i] = string_cluster;
+          }
+        }
+        clusterCollection.insert(cluster_doc, function(err, result) {
+          if (err) {
+            console.log(err);
           }
         });
-        vectors.push(vector);
-        vectorToString[vector] = extraction;
-      });
-
-      // group vectors into k clusters
-      var k = 3;
-      var clusters = clusterfck.kmeans(vectors, k);
-      var cluster_doc = {};
-      for (var i = 0; i < k; i++) {
-        var cluster = clusters[i];
-        var string_cluster = [];
-        cluster.forEach(function(vector) {
-          console.log(vectorToString[vector]);
-          string_cluster.push(vectorToString[vector]);
-        });
-        cluster_doc[i] = string_cluster;
       }
-      clusterCollection.insert(cluster_doc, function(err, result) {
-        if (err) {
-	  console.log(err);
-	}
-        db.close();
-      });
-    }
+    });
   });
 });
 
-function populateDictionary(documents) {
+function populateDictionary(documents, dictionary) {
   documents.forEach(function(extraction_doc) {
     var extraction = extraction_doc.extraction;
     var words = extraction.split(" ");
