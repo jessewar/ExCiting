8f29c472dfd03217dc12b889bd623d569639ad37
Graeme Britz <grbritz@gmail.com>
2015-02-20 11:45:54 -0800

    organized scripts cauase they were getting unwieldy

---
+++ scripts/data_prep/papers_and_authors.py
@@ -0,0 +1,20 @@
+'''
+Script that populates the 'papers' collection using the acl data set.
+'''
+import pymongo
+
+
+def main():
+  # connect to mongod instance
+  client = pymongo.MongoClient()
+
+  # get database
+  db = client.mongodata
+
+
+
+
+
+if __name__ == '__main__':
+  main()
+
