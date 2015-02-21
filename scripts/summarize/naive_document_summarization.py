219e5baaa298bca5344ac07f7416762ca604fa6b
Graeme Britz <grbritz@gmail.com>
2015-02-20 19:58:46 -0800

    sorted results so that they can be compared side by side

--- scripts/summarize/naive_document_summarization.py
+++ scripts/summarize/naive_document_summarization.py
@@ -15,20 +15,27 @@ the words with the highest frequencies in the document and uses those as the
 summaries. It outputs 3 summaries per document.
 '''
 def main():
+  pp = pprint.PrettyPrinter(indent=2)
+  
+  paper_summaries = get_summaries()
+  for paper_id in sorted(paper_summaries):
+    pp.pprint(paper_id)
+    pp.pprint(paper_summaries[paper_id])
+
+  # pp.pprint(get_summaries())
+
+
+def get_summaries():
   summarizer = frequency_summarizer.FrequencySummarizer()
   extractions = get_extractions.above_threshold(25, 're_sentence_extractions')
 
-
   papers_and_summaries = {}
-
   for paper_id in extractions:
     document = ".".join(extractions[paper_id])   
     summaries = summarizer.summarize(document, 3)
     papers_and_summaries[paper_id] = summaries
+  return papers_and_summaries
 
-
-  pp = pprint.PrettyPrinter(indent=2)
-  pp.pprint(papers_and_summaries)
 
 if __name__ == '__main__':
   main()
