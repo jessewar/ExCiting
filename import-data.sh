996b79aa79244e5454037f4be06c6803aa54f4ef
Graeme Britz <grbritz@gmail.com>
2015-03-01 16:44:51 -0800

    renamed sh scripts

--- import-data.sh
+++ import-data.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #Initializes the project by extracting data, running preprocesing, etc
-
 cd data/repo-include
+wget https://www.dropbox.com/s/i9r9akdptyzm6pj/aanrelease2013.tar.gz?dl=0
 tar -xzf aanrelease2013.tar.gz
+rm aanrelease2013.tar.gz
 mv aan ../repo-exclude/
 
 tar -xzf database-dump.tar.gz
