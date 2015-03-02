f177d6395fefd256d45012d90d369f1939dd5e6b
Graeme Britz <grbritz@gmail.com>
2015-03-01 17:14:52 -0800

    updated import script and readme for instructions

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # ExCiting Project
+**Before you push:**
+If you've made any changes to the database, run `sh export-data.sh` so that others will get your updates. 
+
+**After you pull:**
+Run `sh import-data.sh` to get any database updates and to get the aan dataset
 
 
 ## NLTK
--- import-data.sh
+++ import-data.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #Initializes the project by extracting data, running preprocesing, etc
-cd data/repo-include
-wget https://www.dropbox.com/s/i9r9akdptyzm6pj/aanrelease2013.tar.gz?dl=0
-tar -xzf aanrelease2013.tar.gz
-rm aanrelease2013.tar.gz
-mv aan ../repo-exclude/
+cd data
+if [ ! -e repo-exclude/aan ]; then
+  wget https://www.dropbox.com/s/i9r9akdptyzm6pj/aanrelease2013.tar.gz?dl=0
+  tar -xzf aanrelease2013.tar.gz
+  rm aanrelease2013.tar.gz
+  mv aan repo-exclude/
+fi
 
-tar -xzf database-dump.tar.gz
+tar -xzf repo-include/database-dump.tar.gz
 mongorestore database-dump
 rm -rf database-dump
