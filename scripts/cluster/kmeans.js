67692566415376b0e6680e8bfc99bf16cb15fd34
Graeme Britz <grbritz@gmail.com>
2015-03-03 21:08:34 -0800

    refactor on kmeans to only look through papers we have extractions for

--- scripts/cluster/kmeans.js
+++ scripts/cluster/kmeans.js
@@ -7,68 +7,60 @@ var dbPath = "mongodb://localhost/exciting";
 MongoClient.connect(dbPath, function(err, db) {
   var extractionCollection = db.collection("re_sentence_extractions");
   var clusterCollection = db.collection("clusters2");
-  // var papers_above_threshold = fs.readFileSync("../../data/papers_above_threshold.txt", "utf8").split("\n");
+  
+  extractions_cursor = extractionCollection.aggregate([{"$group" : {"_id" : {"cited_paper" : "$cited_paper"}, "extractions" : {"$push" : "$extraction"}}} ], { cursor: { batchSize: 1 }});
 
+  // clusterCollection.drop();
+  extractions_cursor.each(function(error, doc) {
+    if(doc == null) {
+      db.close();
+    }
+    else {
+      var dictionary = [];
+      populateDictionary(doc.extractions, dictionary);
+      var vectors = [];
+      var vectorToString = {};
+      if (!err && doc.extractions.length > 0) {  // must have extractions associated with the paper
+        doc.extractions.forEach(function(extraction) {
+          var vector = [];
+          var extraction_words = extraction.toLowerCase().split(" ");
+          dictionary.forEach(function(dictionary_word) {
+            if (extraction_words.indexOf(dictionary_word) >= 0) {
+              vector.push(1);
+            } else {
+              vector.push(0);
+            }
+          });
+          vectors.push(vector);
+          vectorToString[vector] = extraction;
+        });
 
-  var papers_collection = db.collection("papers");
-
-  papers_collection.find().toArray(function(err, documents) {
-    documents.forEach(function(document) {
-      paper_id = document.paper_id;
-      
-      extractionCollection.find({cited_paper:paper_id}).toArray(function(err, documents) {
-        var dictionary = [];
-        populateDictionary(documents, dictionary);
-        var vectors = [];
-        var vectorToString = {};
-        if (!err && documents.length > 0) {  // must have extractions associated with the paper
-          documents.forEach(function(extraction_doc) {
-            var vector = [];
-            var extraction = extraction_doc.extraction;
-            var extraction_words = extraction.toLowerCase().split(" ");
-            dictionary.forEach(function(dictionary_word) {
-              if (extraction_words.indexOf(dictionary_word) >= 0) {
-                vector.push(1);
-              } else {
-                vector.push(0);
-              }
+        // group vectors into k clusters
+        var k = 2;
+        var clusters = clusterfck.kmeans(vectors, k);
+        var cluster_doc = {"cited_paper": doc['_id']['cited_paper']};
+        for (var i = 0; i < k; i++) {
+          var cluster = clusters[i];
+          var string_cluster = [];
+          if (cluster != undefined) {
+            cluster.forEach(function(vector) {
+              string_cluster.push(vectorToString[vector]);
             });
-            vectors.push(vector);
-            vectorToString[vector] = extraction;
-          });
-
-          // group vectors into k clusters
-          var k = 2;
-          var clusters = clusterfck.kmeans(vectors, k);
-          var cluster_doc = {"cited_paper":paper_id};
-          for (var i = 0; i < k; i++) {
-            var cluster = clusters[i];
-            var string_cluster = [];
-            if (cluster != undefined) {
-              cluster.forEach(function(vector) {
-                string_cluster.push(vectorToString[vector]);
-              });
-              cluster_doc[i] = string_cluster;
-            }
+            cluster_doc[i] = string_cluster;
           }
-          clusterCollection.insert(cluster_doc, function(err, result) {
-            if (err) {
-              console.log(err);
-            }
-          });
         }
-      });
-    });
-
-    // db.close()
-
-    console.log("done");
+        clusterCollection.insert(cluster_doc, function(err, result) {
+          if (err) {
+            console.log(err);
+          }
+        });
+      }
+    }
   });
 });
 
-function populateDictionary(documents, dictionary) {
-  documents.forEach(function(extraction_doc) {
-    var extraction = extraction_doc.extraction;
+function populateDictionary(extractions, dictionary) {
+  extractions.forEach(function(extraction) {
     var words = extraction.split(" ");
     words.forEach(function(word) {
       word = word.toLowerCase();
